62e7bfc7d38ad4d2c75f2bad6ca5e4760635ff4f
JHOSEPH-CACERES <jhoseph@localhost.localdomain>
2024-02-05 23:21:33 -0400

    Reto de Python de Codigo facilito Dia 1

---
+++ main.py
@@ -0,0 +1,9 @@
+#RETO PYTHON DIA 1
+
+nombre = input("ingrese el nombre\n")
+apellido = input("ingrese el apellido\n")
+numero = input("ingrese el numero telefonico\n")
+correo = input("ingrese el correo electronico\n")
+
+print("")
+print("hola "+nombre+" "+apellido+" "+"en breve recibiras un correo a "+correo)
